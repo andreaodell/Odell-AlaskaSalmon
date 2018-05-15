6b8658c2e2e42a72e8bcc495a955213721890a58
AndreaOdell <dreaodell96@gmail.com>
2018-05-15 14:12:15 -0700

    Modified Data - A and C creek return times vs distance

--- scripts/Odell-AlaskaSalmon.R
+++ scripts/Odell-AlaskaSalmon.R
@@ -1,4 +1,30 @@
 
 ### Read in file and run packages
-master <- read.csv("data/autocorrelation_sections_corrected_051118_final.csv")
+master <- read.csv("data/master_duplicate_removed.csv")
+distances <- read.csv("data/creek_midpoint.csv")
 library(tidyverse)
+
+## adding a column for distance of fish based on location_section
+master_with_distance <-  cbind(master,midpoint=rep(master$location_section))
+### replace section location values in midpoint column with midpoint values.
+master_with_distance$midpoint <- distances$midpoint[match(master_with_distance$midpoint, distances$section)]
+
+
+### Looking at return times and distance in each year between each creek
+
+# A creek fish in 2004 
+acreek_fish_2004_return_time <- master %>% 
+  filter(Year == "2004") %>% 
+  group_by(Tag) %>% 
+  filter(Date == first(Date)) %>% 
+  filter(Location == "a")
+
+# C creek fish in 2004
+ccreek_fish_2004_return_time <- master %>% 
+  filter(Year == "2004") %>% 
+  group_by(Tag) %>% 
+  filter(Date == first(Date)) %>% 
+  filter(Location == "c")
+
+
+
