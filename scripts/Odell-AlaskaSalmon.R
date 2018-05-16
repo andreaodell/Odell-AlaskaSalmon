2457696abac6058bacca3214d07cae246313fbb6
AndreaOdell <dreaodell96@gmail.com>
2018-05-16 14:35:16 -0700

    proposed more questions with answers using graphs including: Fish creek stay length and fish per section per year

--- scripts/Odell-AlaskaSalmon.R
+++ scripts/Odell-AlaskaSalmon.R
@@ -1,6 +1,6 @@
 
 ### Read in file and run packages
-master <- read.csv("data/autocorrelation_sections_corrected_051118_final.csv.csv")
+master <- read.csv("data/autocorrelation_sections_corrected_051118_final.csv", na.strings = "NA")
 distances <- read.csv("data/creek_midpoint.csv")
 library(tidyverse)
 
@@ -38,13 +38,13 @@ ggplot(fish_per_year, aes(x = Year, color = Location)) +
   geom_histogram()
 
 
-#####################################################################
-#####################################################################
+######################################################################
+######################################################################
 
-#Looking at return times and distance in each year between each creek
+#Looking at return times and distance in each year between each creek#
 
-#####################################################################
-#####################################################################
+######################################################################
+######################################################################
 
 
 # A creek fish in *year* 
@@ -62,8 +62,43 @@ ccreek_fish_2010_return_time <- master_with_distance %>%
   filter(Location == "c")
 
 ## create a scatter plot graph
-ggplot(acreek_fish_2005_return_time, aes(x = Date, y = midpoint, color = Sex)) +
+ggplot(acreek_fish_2005_last_day, aes(x = Date, y = midpoint, color = Sex)) +
   geom_point()    # plot for a creek  - edit name to match year you want
 ggplot(ccreek_fish_2010_return_time, aes(x = Date, y = midpoint, color = Sex)) +
   geom_point()    # plot for c creek - edit name to match year you want
+
+
+
+#################################################
+# How long do fish typically stay in the creek? #
+#################################################
+
+fish_creek_stay_length <- master %>% 
+  select(Year_Tag, Sex) %>% 
+  group_by(Year_Tag) %>% 
+  summarise("" = n()) 
+
+
+ggplot(fish_creek_stay_length, aes(x = count)) +
+  geom_histogram() +
+  labs(x = "days") +
+  scale_x_continuous(limits = c(0, 50))
+
+
+
+######################################
+# How many fish per section each year#
+######################################
+
+location_count_2004_ccreek <- master %>% 
+  select(Year, Location, location_section) %>% 
+  filter(Year == 2004, Location == "c" ) %>% 
+  group_by(location_section) %>% 
+  summarise("number_of_fish" = n())
+
+ggplot(location_count_2004_ccreek, aes(x = location_section, y = number_of_fish)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust =0.75))
+
+
 
